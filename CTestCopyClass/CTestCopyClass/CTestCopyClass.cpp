f0139715eed0e9df8f4ede9f0b7966454d787327
ciecimac <ciecclm@imnu.edu.cn>
2019-05-20 16:37:05 +0800

    浅拷贝

--- CTestCopyClass/CTestCopyClass/CTestCopyClass.cpp
+++ CTestCopyClass/CTestCopyClass/CTestCopyClass.cpp
@@ -6,51 +6,50 @@
 using namespace std;
 class CTestCopy
 {
-	int x;
-	int y;
+	int *x;
 public:
 	CTestCopy();
-	CTestCopy(int a, int b);
+	CTestCopy(int a);
 	CTestCopy(CTestCopy & om);
-	void setXY(int a, int b);
+	void setX(int a);
 	void disPlay();
 	~CTestCopy();
 };
 CTestCopy::CTestCopy()
 {
-	x = 0;
-	y = 0;
-	cout << "默认构造函数 x=" << x << " y=" << y << endl;
+	x = new int;
+	*x = 0;
+	cout << "默认构造函数 *x=" << *x << "          x变量的值="<<x<<"    x变量的地址="<<&x<< endl;
 }
 CTestCopy::~CTestCopy()
 {
-	cout << "析构函数   x=" << x << " y=" << y << endl;
+	cout << "析构函数 *x=" << *x << "          x变量的值=" << x << "    x变量的地址=" << &x << endl;
+	delete x;
 }
-CTestCopy::CTestCopy(int a, int b)
+CTestCopy::CTestCopy(int a)
 {
-	x = a;
-	y = b;
-	cout << "构造函数 x=" << x << " y=" << y << endl;
+	x = new int;
+	*x = a;
+	cout << "构造函数 *x=" << *x << "          x变量的值=" << x << "    x变量的地址=" << &x << endl;
 }
 CTestCopy::CTestCopy(CTestCopy & om)
 {
-	x = om.x * 2;
-	y = om.y * 2;
-	cout << "复制构造函数 x=" << x << " y=" << y << endl;
+	x = om.x;
+	cout << "复制构造函数 *x=" << *x << "          x变量的值=" << x << "    x变量的地址=" << &x << endl;
 }
 
-void CTestCopy::setXY(int a, int b)
+void CTestCopy::setX(int a)
 {
-	x = a;
-	y = b;
+	*x = a;
+
 }
 void CTestCopy::disPlay()
 {
-	cout << "display    x=  " << x << " y= " << y << endl;
+	cout << "显示函数 *x=" << *x << "          x变量的值=" << x << "    x变量的地址=" << &x << endl;
 }
 CTestCopy myTest()
 {
-	CTestCopy om(600,700);
+	CTestCopy om(600);
 	om.disPlay();
 	return om;
 }
@@ -60,10 +59,9 @@ int testtemp(int x)
 }
 int main()
 {
-	CTestCopy ot;
+	CTestCopy ot(100);
 	ot.disPlay();
-	ot = myTest();
-	ot.disPlay();
+	CTestCopy obj(ot);
     return 0;
 }
 
